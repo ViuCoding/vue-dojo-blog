89c087b235b5f19c5ca68f9e8f929014870ae6b9
Vincenzo- <viulenzgg@gmail.com>
2022-07-14 18:39:37 +0200

    Using REFS to make data in the setup() hook Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">Home</div>
   <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">CLICK</button>
+  <button @click="age++">CLICK TO ADD 1 TO AGE</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
@@ -12,22 +14,24 @@ export default {
   name: "HomeView",
   setup() {
     // in the setup function we can write regular JS
-    const p = ref(null);
+    // const p = ref(null);
 
-    let name = "Mario";
-    let age = 41;
+    // We access the ref value using variableName.value
+    const name = ref("Mario");
+
+    const age = ref(41);
 
     const handleClick = () => {
-      console.log("You clicked me!");
-      console.log(p, p.value);
-      p.value.classList.add("test");
-      p.value.innerHTML = "Hello NINJASSSS!";
+      // This will update the name variable but the change won't be reflected in the template since we are using the Composition API.
+      // In order to have reactie data we need to use REFS
+      name.value = "Luigi";
+      console.log(name);
     };
 
     // In order to have access to these variables in the template we need to return them inside of an object.
     // Keep in mind, this is different than using the data() function. The data inside the setup() function is NOT reactive. So if a the value of "name" changes it WILL NOT be reflected in the template.
 
-    return { name, age, handleClick, p };
+    return { name, age, handleClick };
     // this is the same of writing return {name: name, age: age, handleClick : handleClick}
   },
 };
