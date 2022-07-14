1f859b7431b82efd1c12eaf43a6dc505e1d00ddd
Vincenzo- <viulenzgg@gmail.com>
2022-07-14 18:16:08 +0200

    Composition API basics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">Home</div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">CLICK</button>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    // in the setup function we can write regular JS
+    let name = "Mario";
+    let age = 41;
+
+    const handleClick = () => {
+      console.log("You clicked me!");
+    };
+
+    // In order to have access to these variables in the template we need to return them inside of an object.
+    // Keep in mind, this is different than using the data() function. The data inside the setup() function is NOT reactive. So if a the value of "name" changes it WILL NOT be reflected in the template.
+
+    return { name, age, handleClick };
+    // this is the same of writing return {name: name, age: age, handleClick : handleClick}
+  },
+};
 </script>
