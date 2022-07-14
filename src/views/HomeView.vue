f64ed72022dc1c2e315e2f6c45b8eb378776069e
Vincenzo- <viulenzgg@gmail.com>
2022-07-14 18:24:51 +0200

    Template Refs using Composition API, (I don't know WTF is going on)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,33 @@
 <template>
   <div class="home">Home</div>
-  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">CLICK</button>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 // @ is an alias to /src
 
 export default {
   name: "HomeView",
   setup() {
     // in the setup function we can write regular JS
+    const p = ref(null);
+
     let name = "Mario";
     let age = 41;
 
     const handleClick = () => {
       console.log("You clicked me!");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.innerHTML = "Hello NINJASSSS!";
     };
 
     // In order to have access to these variables in the template we need to return them inside of an object.
     // Keep in mind, this is different than using the data() function. The data inside the setup() function is NOT reactive. So if a the value of "name" changes it WILL NOT be reflected in the template.
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
     // this is the same of writing return {name: name, age: age, handleClick : handleClick}
   },
 };
